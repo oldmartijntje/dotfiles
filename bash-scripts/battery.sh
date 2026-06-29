40f8700e5f9d071959d9bff75950f2c9aafdeb33
oldmartijntje <oldmartijntje@gmail.com>
2026-06-29 14:30:26 +0200

    also report battery full

--- bash-scripts/battery.sh
+++ bash-scripts/battery.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 while true; do
   bat_lvl=$(cat /sys/class/power_supply/BAT0/capacity)
+  bat_status=$(cat /sys/class/power_supply/BAT0/status)
   if [ "$bat_lvl" -le 15 ]; then
-    notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
+    notify-send --urgency=critical "Battery Low" "Level: ${bat_lvl}%"
+    sleep 1200
+  elif [ "$bat_lvl" -ge 90 ] && [ "$bat_status" = "Charging" ]; then
+    notify-send --urgency=normal "Battery Full" "Level: ${bat_lvl}%"
     sleep 1200
   else
     sleep 120
